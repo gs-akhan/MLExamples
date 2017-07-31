388dc6f179480d9abc2b63c58779033aff015749
Azharuddin <akhan@gainsight.com>
2017-07-31 19:10:28 +0530

    First commit

---
+++ 1.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+ls = np.array([1,4,4,5,6,6])
+def azhar() :
+    print "Azhar is here"
+    
+r = np.arange(100)
+p = np.arange(100)
+
+# z = r + p
+# for k in z:
+#     print k
+
+azhar()
+
+print r[r.size - 1]
+
+
